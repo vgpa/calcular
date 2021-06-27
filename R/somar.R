09cca0f297a5adf6f5a91e99a9bc7ad1f3ca79ab
vanessa galvão <estefsni@gmail.com>
2021-06-27 19:29:49 -0300

    first commit

---
+++ R/somar.R
@@ -0,0 +1,13 @@
+#' Este programa apara somar dois numeros
+#'
+#'@param a
+#'@param b
+#'
+#'@examples
+#'este pacote somar a + b
+#'
+#'@export
+#'@export
+somar <- function(a,b){
+  a + b
+}
---
+++ R/subtrair.R
@@ -0,0 +1,12 @@
+#' Este programa apara somar dois numeros
+#'
+#'@param a um numemro
+#'@param b outro numero
+#'
+#'@examples
+#'este pacote subtrais  a + b
+#'
+#'@export
+somar <- function(a,b){
+  a + b
+}
